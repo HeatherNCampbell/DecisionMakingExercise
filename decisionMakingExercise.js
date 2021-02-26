a2eee01411ffb9a43e899417735030fe428f14a2
hcampbell1212 <heather.campbell1212@gmail.com>
2021-02-26 16:23:34 -0600

    DecisionMakingExercise

---
+++ decisionMakingExercise.js
@@ -0,0 +1,46 @@
+//  1a
+let randomNum = Math.random(); 
+// console.log(randomNum);
+randomNum *= 5;
+// console.log(randomNum);
+randomNum = Math.floor(randomNum);
+// console.log(randomNum);
+randomNum++;
+console.log(randomNum);
+
+//  1b
+if (randomNum >= 4) {
+    console.log("Greater than or equal to 4");
+}
+else if (randomNum > 1) {
+    console.log("Equal to 2 or 3");
+}
+    else {
+console.log("Equal to 1");
+}
+
+//  1c
+if (randomNum != 3) {
+    console.log("NOT equal to 3");
+}
+
+//  1d
+if (randomNum != 3 && randomNum != 5) {
+    console.log("NOT equal 3 AND not equal to 5");
+}
+
+//  1e
+if (randomNum == 2 || randomNum == 4) {
+    console.log("Equal to 2 or equal to 4");
+}
+
+
+
+
+
+
+
+
+
+
+// 1b
